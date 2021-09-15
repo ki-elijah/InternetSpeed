22283c0cbf403a572d70b8daed00f92e52be6d84
elabs <elijahknsubuga@gmail.com>
2021-09-15 09:49:45 +0300

    first commit

---
+++ internet.py
@@ -0,0 +1,9 @@
+from speedtest import Speedtest
+
+st = Speedtest()
+
+st.get_best_server()
+print(f"Your ping is: {st.results.ping} ms")
+print(f"Your download speed: {round(st.download() / 1000 / 1000, 1)} Mbit/s")
+print(f"Your upload speed: {round(st.upload() / 1000 / 1000, 1)} Mbit/s")
+#....
